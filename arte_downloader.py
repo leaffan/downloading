dd079d1d727a2dbed017f6ac03ae95cef2e97360
leaffan <leaffan@gmx.net>
2017-01-24 15:47:20 +0100

    Modify ARTE downloader to allow for command line execution

--- arte_downloader.py
+++ arte_downloader.py
@@ -5,10 +5,7 @@ import os
 import sys
 import re
 import argparse
-from urllib.parse import urlparse, parse_qs
 from datetime import datetime
-
-
 
 import requests
 from dateutil import parser
@@ -23,10 +20,15 @@ class ArtePlus7Downloader():
     FORMAT = "MP4"
     QUALITY_KEYS = {'high': 'SQ', 'medium': 'MQ', 'low': 'EQ'}
     DOWNLOAD_CHUNK_SIZE = 65536
+    BROADCAST_ID_REGEX = re.compile("\d{6}\-\d{3}")
 
-    def __init__(self, tgt_dir, language='de', quality='low'):
+    def __init__(self, tgt_dir, urls_media_ids, language='de', quality='low'):
         # setting target directory
         self.tgt_dir = tgt_dir
+
+        # retrieving broadcast ids from specified string with urls and/or
+        # broadcast ids
+        self.broadcast_ids = self.retrieve_broadcast_ids(urls_media_ids)
 
         # setting video quality to be downloaded
         if quality.lower() in self.QUALITY_KEYS:
@@ -35,21 +37,35 @@ class ArtePlus7Downloader():
             self.quality = 'MQ'
 
         # setting video language to be downloaded
-        if language.lower() == 'fr':
-            self.language = '2'
-        else:
+        if language.lower() == 'de':
             self.language = '1'
+        else:
+            self.language = '2'
 
-        # combining key
+        # combining quality and language to unique key
         self.video_key = "_".join((
             self.PROTOCOL, self.FORMAT, self.quality, self.language))
 
-    def download(self, url):
+    def download_all(self):
         """
-        Downloads video from specified url.
+        Downloads videos for all registered broadcast ids.
+        """
+        for broadcast_id in self.broadcast_ids:
+            self.download(broadcast_id)
+
+    def download(self, broadcast_id):
+        """
+        Downloads video for specified broadcast id.
         """
         # retrieving json struct with video information
-        json_data = self.retrieve_json_struct(url)
+        json_data = self.retrieve_json_struct(broadcast_id)
+
+        if 'custom_msg' in json_data['videoJsonPlayer']:
+            print(
+                "+ Unable to retrieve video information for broadcast_id %s"
+                % broadcast_id)
+            return
+
         # setting up local file name for downloaded video
         file_name = self.build_file_name(json_data)
         # downloading video to local file
@@ -67,8 +83,15 @@ class ArtePlus7Downloader():
         # otherwise using current date
         except:
             broadcast_date = datetime.now()
-        # retrieving broadcast title
-        broadcast_title = json_data['videoJsonPlayer']['VST']['VNA']
+
+        # trying to retrieve broadcast title
+        try:
+            broadcast_title = json_data['videoJsonPlayer']['VST']['VNA']
+        # otherwise using alternative broadcast_title
+        except:
+            broadcast_title = json_data['videoJsonPlayer']['VTI']
+            broadcast_title = broadcast_title.lower().replace(" ", "_")
+
         # combining broadcast date and title to resulting title
         return "_".join((
             broadcast_date.strftime("%Y-%m-%d"),
@@ -89,10 +112,10 @@ class ArtePlus7Downloader():
         # downloading video data
         with open(tgt_path, 'wb') as handle:
             sum_bytes = 0
-            print("+ Downloading %d kB to %s:" % (tgt_bytes / 1024, tgt_path))
+            print("+ Downloading %d kB to %s" % (tgt_bytes / 1024, tgt_path))
 
             for block in r.iter_content(self.DOWNLOAD_CHUNK_SIZE):
-                if not block:
+                if not block:  # or sum_bytes > 500000:
                     break
                 handle.write(block)
                 sum_bytes += self.DOWNLOAD_CHUNK_SIZE
@@ -103,46 +126,42 @@ class ArtePlus7Downloader():
             else:
                 print()
 
-    def retrieve_json_struct(self, url):
+    def retrieve_json_struct(self, broadcast_id):
         """
         Retrieves json struct with all necessary information for video dowload
         from specified url.
         """
-        broadcast_id = self.retrieve_broadcast_id(url)
         json_url = "".join((self.JSON_PREFIX, broadcast_id, self.JSON_SUFFIX))
         r = requests.get(json_url)
         return r.json()
 
-    def retrieve_broadcast_id(self, url):
+    def retrieve_broadcast_ids(self, urls_or_ids):
         """
-        Retrieves broadcast id from specified url.
+        Converts items from specified string into valid broadcast ids.
         """
-        url_query_component = urlparse(url).query
-        try:
-            broadcast_id = parse_qs(
-                url_query_component)[self.BROADCAST_ID_KEY].pop()
-        except KeyError:
-            return None
-        return broadcast_id
+        broadcast_ids = list()
+
+        raw_urls_or_ids = [s.strip() for s in urls_or_ids.split(",")]
+
+        for url_or_id in raw_urls_or_ids:
+            match = re.search(self.BROADCAST_ID_REGEX, url_or_id)
+            if match:
+                broadcast_ids.append(match.group(0))
+
+        return broadcast_ids
 
 
 if __name__ == '__main__':
 
-    arg_parser = argparse.ArgumentParser(description='Download videos from ARTE media library.')
-    arg_parser.add_argument('-d', '--directory', dest='tgt_dir', metavar='target_directory', required=True, help='Target directory for video downloads.')
-    arg_parser.add_argument('urls_or_media_ids', metavar='video_urls/media_ids', help='Comma-separated list of video urls or media ids.')
-
+    arg_parser = argparse.ArgumentParser(
+        description='Download videos from ARTE media library.')
+    arg_parser.add_argument(
+        '-d', '--directory', dest='tgt_dir', metavar='target_directory',
+        required=True, help='Target directory for video downloads.')
+    arg_parser.add_argument(
+        'urls_or_media_ids', metavar='video_urls/media_ids',
+        help='Comma-separated list of video urls or media ids.')
     args = arg_parser.parse_args()
 
-    sys.exit()
-
-    tgt_dir = r"D:\dlds\vi"
-
-    urls = list()
-    urls.append("http://www.arte.tv/guide/de/plus7/?em=063711-001")
-    urls.append("http://www.arte.tv/guide/de/plus7/?em=067846-011")
-
-    dl = ArtePlus7Downloader(tgt_dir)
-
-    for url in urls:
-        dl.download(url)
+    dl = ArtePlus7Downloader(args.tgt_dir, args.urls_or_media_ids)
+    dl.download_all()
